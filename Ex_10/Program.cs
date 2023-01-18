2b4e9b0402c904f987e9a7be1636b82dbb3ffb61
pidzhak <pidzhak84@gmail.com>
2023-01-18 22:41:54 +0300

    Решение Задачи 10

--- Ex_10/Program.cs
+++ Ex_10/Program.cs
@@ -3,3 +3,17 @@
 и на выходе показывает вторую цифру этого числа (456 -> 5)
 */
 
+Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SecondDigit (int number)
+{
+    int digit23 = number % 100;
+    int result = digit23 / 10;
+    return result;
+}
+
+int XXX = SecondDigit(number);
+Console.WriteLine($"Вторая цифра этого трехзначного числа {XXX} ");
